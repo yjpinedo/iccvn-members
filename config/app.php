8ca4e2da9c75ca23e7410287949d6a2f5657dada
Yeimer Piendo <yjpinedo@unicesar.edu.co>
2021-12-15 21:11:59 -0500

    Spatie Permission

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
 
+        // Permission
+        Spatie\Permission\PermissionServiceProvider::class,
     ],
 
     /*
